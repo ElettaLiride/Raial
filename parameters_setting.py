d683f02590299b38ff8e1b50682398fe4a1131bf
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-24 12:45:00 +0200

    correct get_assignement parameter

--- parameters_setting.py
+++ parameters_setting.py
@@ -5,7 +5,7 @@ import debugging_tools as db
 def changing_parameters(parameters, table, sector=4, layer=0, component=0):
     if isinstance(table, pd.DataFrame):
             if isinstance(parameters, list):
-                table.loc[((table['sector'] == sector) & (table['layer'] == layer) & (table['component'] == component)), table.columns.to_list()] = [sector, layer, component] + parameters
+                table.loc[((table['sector'] == sector) & (table['layer'] == layer) & (table['component'] == component)), table.columns.tolist()] = [sector, layer, component] + parameters
             else:
                 print('some problem here: line {}'.format(db.line_numb()))
                 raise ValueError
