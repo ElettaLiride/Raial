a53b9c459c77613ff9429ec3c16ad42d41d92630
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-24 12:33:04 +0200

    update parameters

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__=="__main__":
 
     provider = cc.connecting_ccdb(calibration_connection)
     old_pars_table = cc.reading_ccdb(provider, calibration_table, variation)
-    new_pars_table = pm.changing_parameters(pars, calibration_table, module[0], module[1], module[2])
+    new_pars_table = pm.changing_parameters(pars, old_pars_table, module[0], module[1], module[2])
     cc.adding_to_ccdb(pars, provider, calibration_table, variation)
 
     print(pars)
