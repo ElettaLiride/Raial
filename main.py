038fa499396367071c322ae43e9073a9dc8db72b
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-24 15:40:02 +0200

    trying different variation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-import pandas as pd
-import numpy as np
+import os
+import subprocess
 
 import ccdb_connection as cc
 import parameters_setting as pm
@@ -8,10 +8,15 @@ import parameters_setting as pm
 if __name__=="__main__":
     calibration_connection = "sqlite:////work/clas12/users/costantini/RICH_alignement/Costantini_script/ccdb_4.3.2.sqlite"
     calibration_table      = '/calibration/rich/misalignments'
-    variation              = 'default'
+    variation              = 'misalignements'
     user                   = "Costantini"
     module = [4,201,1]
     pars = [0,0,1,1,1,0]
+    RunNumber = "0"
+    Layer = "-1"
+    filetofilter = "file"
+
+    ## update ccdb
 
     provider = cc.connecting_ccdb(calibration_connection)
     old_pars_table = cc.reading_ccdb(provider, calibration_table, variation)
@@ -20,17 +25,11 @@ if __name__=="__main__":
     toadd = new_pars_table.values.tolist()
     cc.adding_to_ccdb(toadd, provider, calibration_table, variation)
 
-    print(pars)
-    # ##Reading environment variable
-    #
-    # MIRAZITAsrc = os.environ()
-    # MIRAZITAfil = os.environ()
-    #
     # ##executing Mirazita code for filtering
     # if sys.argv[0] == '-f':
-    #     subprocess.run([])
+    #     subprocess.run(["./mirazita_code/RichAI_FilterC/filterHipo", "-R" + RunNumber, "-L" + Layer, filetofilter] )
     #
     # ##executing Mirazita code for Reconstruction and comparison
-    # subprocess.run([])
+    # subprocess.run(["/mirazita_code/RichAI_script/run.sh", "list.file"])
     #
     # ##execute Costantini code for update ccdb
