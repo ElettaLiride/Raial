01649fcc39214d6c6c74117b826c9588cdd7288a
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-25 11:50:19 +0200

    preparing for interation

--- parameters_setting.py
+++ parameters_setting.py
@@ -4,24 +4,36 @@ import debugging_tools as db
 
 def changing_parameters1(parameters, table, sector=4, layer=0, component=0):
     if isinstance(table, pd.DataFrame):
-            if isinstance(parameters, list):
-                table.loc[((table['sector'] == sector) & (table['layer'] == layer) & (table['component'] == component)), table.columns.tolist()] = [sector, layer, component] + parameters
-            else:
-                print('some problem here: line {}'.format(db.line_numb()))
-                raise ValueError
+        if isinstance(parameters, list):
+            table.loc[((table['sector'] == sector) & (table['layer'] == layer) & (
+                        table['component'] == component)), table.columns.tolist()] = [sector, layer,
+                                                                                      component] + parameters
+        else:
+            print('some problem here: line {}'.format(db.line_numb()))
+            raise ValueError
     else:
         print('some problem here: line {}'.format(db.line_numb()))
         raise ValueError
 
     return table
 
-def changing_parameters(parameters, table, sector=4, layer=0, component=0):
+
+def changing_parameters(parameters, table, module=None):
+    if module is None:
+        module = [4, 0, 0]
+
+    sector = module[0]
+    layer = module[1]
+    component = module[2]
+
     if isinstance(table, pd.DataFrame):
-            if isinstance(parameters, list):
-                table.loc[((table['sector'] == sector) & (table['layer'] == layer) & (table['component'] == component)), table.columns.tolist()] = [sector, layer, component] + parameters
-            else:
-                print('some problem here: line {}'.format(db.line_numb()))
-                raise ValueError
+        if isinstance(parameters, list):
+            table.loc[((table['sector'] == sector) & (table['layer'] == layer) & (
+                        table['component'] == component)), table.columns.tolist()] = [sector, layer,
+                                                                                      component] + parameters
+        else:
+            print('some problem here: line {}'.format(db.line_numb()))
+            raise ValueError
     else:
         print('some problem here: line {}'.format(db.line_numb()))
         raise ValueError
