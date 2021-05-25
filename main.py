2258403d6eabf400b01fd876104a26a7c3b6b009
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-25 15:22:00 +0200

    trying to run the whole code first try plus comment

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ import subprocess
 import ccdb_connection as cc
 import parameters_setting as pm
 
+exereco = "/work/clas12/users/devita/clas12validation/clara-iss643-rich/plugins/clas12/bin/recon-util"
+exeplot = "mirazita_code/RichAI_Plots/richPlots"
+fileIN = "../RICH_alignement/Mirazita_script/RichAI_FilterC/rec_clas_5208_AIskim1.hipo"
+fileOUT = "5208_AIskim1.hipo"
+DrawingFile = "mirazita_code/RichAI_plots/DrawRichPlots.C"
+yalm = "mirazita_code/RichAI_script/rich.yaml"
+dirOUT = "out/"
+
+calibration_connection = "sqlite:////$PWD/ccdb_4.3.2.sqlite "
+calibration_table = '/calibration/rich/misalignments'
+variation = 'misalignements'
+user = "Costantini"
+
 def getrunnumber(file):
     return file.split("_")[2]
 
+runnumber = getrunnumber(fileIN)
 
 if __name__ == "__main__":
-    exereco = "/work/clas12/users/devita/clas12validation/clara-iss643-rich/plugins/clas12/bin/recon-util"
-    exeplot = "mirazita_code/RichAI_Plots/richPlots"
-    fileIN = "../RICH_alignement/Mirazita_script/RichAI_FilterC/rec_clas_5208_AIskim1.hipo"
-    fileOUT = "5208_AIskim1.hipo"
-    runnumber = getrunnumber(fileIN)
-    DrawingFile = "mirazita_code/RichAI_plots/DrawRichPlots.C"
-    yalm = "mirazita_code/RichAI_script/rich.yaml"
-    dirOUT = "out/"
 
-    calibration_connection = "sqlite:////$PWD/ccdb_4.3.2.sqlite "
-    calibration_table = '/calibration/rich/misalignments'
-    variation = 'misalignements'
-    user = "Costantini"
 
     module = [4, 201, 1]
     pars = [0, 0, 1, 1, 1, 0]
@@ -37,14 +39,18 @@ if __name__ == "__main__":
 
     # executing reconstruction
     # subprocess.run(["./", exereco, "-i", fileIN, "-o", fileOUT, "-y", yalm])
+
     bashCommand = "/work/clas12/users/devita/clas12validation/clara-iss643-rich/plugins/clas12/bin/recon-util -i " + fileIN + " -o " + fileOUT + " -y " + yalm
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    print(output)
+
     #execute evaluation
     # subprocess.run([exeplot, "-R"+runnumber, dirOUT + "/*"])
     bashCommand = "./"+exeplot+" -R"+runnumber+" "+dirOUT+"/*"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    print(output)
     #execute drawing
 
     # subprocess.run(["root", "-l", "-p", "-q", DrawingFile + "(\"RichPlots_" + runnumber + "\")"])
