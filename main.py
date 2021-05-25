1d98bd2a5698d8fb66db38a3262f40c5562e0276
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-25 11:56:15 +0200

    trying connection

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ import subprocess
 import ccdb_connection as cc
 import parameters_setting as pm
 
-if __name__=="__main__":
-    calibration_connection = "sqlite:////work/clas12/users/costantini/RICH_alignement/Costantini_script/ccdb_4.3.2.sqlite"
-    calibration_table      = '/calibration/rich/misalignments'
-    variation              = 'misalignements'
-    user                   = "Costantini"
+if __name__ == "__main__":
+    calibration_connection = "sqlite:///$PWD/ccdb_4.3.2.sqlite "
+    calibration_table = '/calibration/rich/misalignments'
+    variation = 'misalignements'
+    user = "Costantini"
 
-    module = [4,201,1]
+    module = [4, 201, 1]
     pars = [0, 0, 1, 1, 1, 0]
 
     RunNumber = "0"
     Layer = "-1"
     filetofilter = "file"
 
-    ## update ccdb
+    # executing Mirazita code for filtering
+    if sys.argv[0] == '-f':
+        subprocess.run(["./mirazita_code/RichAI_FilterC/filterHipo", "-R" + RunNumber, "-L" + Layer, filetofilter])
+
+    # executing Mirazita code for Reconstruction and comparison
+    subprocess.run(["/mirazita_code/RichAI_script/run.sh", "list.file"])
+
+    # execute Costantini code for update ccdb
+    #  update ccdb
 
     provider = cc.connecting_ccdb(calibration_connection, variation)
     old_pars_table = cc.reading_ccdb(provider, calibration_table, variation)
@@ -26,12 +34,3 @@ if __name__=="__main__":
 
     toadd = new_pars_table.values.tolist()
     cc.adding_to_ccdb(toadd, provider, calibration_table, variation)
-
-    # ##executing Mirazita code for filtering
-    # if sys.argv[0] == '-f':
-    #     subprocess.run(["./mirazita_code/RichAI_FilterC/filterHipo", "-R" + RunNumber, "-L" + Layer, filetofilter] )
-    #
-    # ##executing Mirazita code for Reconstruction and comparison
-    # subprocess.run(["/mirazita_code/RichAI_script/run.sh", "list.file"])
-    #
-    # ##execute Costantini code for update ccdb
