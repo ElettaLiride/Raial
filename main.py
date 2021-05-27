ecc7cd57b6afa2e034f05c0f619d8b4fd859ef90
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-27 12:21:54 +0200

    mail update (all run in series)

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import run_filter
 if __name__ == "__main__":
     ## INIT CCDB
 
-    calibration_connection = "sqlite:///$PWD/ccdb_4.3.2.sqlite"
+    calibration_connection = "sqlite:////work/clas12/users/costantini/RICH_alignement/Costantini_script/ccdb_4.3.2.sqlite"
     calibration_table = "/calibration/rich/misalignments"
     variation = "misalignments"
     user = "Costantini"
