bbaf11ce48809d172579237a0a3cff74a246fea4
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-27 15:14:29 +0200

    mail update (all run in series)

--- costantini_code/parameters_setting.py
+++ costantini_code/parameters_setting.py
@@ -4,7 +4,12 @@ import tools as db
 
 def changing_parameters1(parameters, table, sector=4, layer=0, component=0):
     if isinstance(table, pd.DataFrame):
+        print("dentro primo if")
         if isinstance(parameters, list):
+            table['sector']=table['sector'].astype(int)
+            table['layer']=table['layer'].astype(int)
+            table['component']=table['component'].astype(int)
+
             table.loc[((table['sector'] == sector) & (table['layer'] == layer) & (
                         table['component'] == component)), table.columns.tolist()] = [sector, layer,
                                                                                       component] + parameters
