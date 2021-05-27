3832a864bd4288d77fdc5f17807236fc8492ef3c
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-27 12:39:59 +0200

    mail update (all run in series)

--- run_reco.py
+++ run_reco.py
@@ -12,7 +12,7 @@ def runcommand(fileIN, fileOUT=None, yalm="mirazita_code/RichAI_script/rich.yaml
     runnumber = t.getrunnumber(f)
 
 
-    command = "/work/clas12/users/devita/clas12validation/clara-iss643-rich/plugins/clas12/bin/recon-util -i " + f + " -o " + fileOUT + " -y " + yalm
+    command = "/work/clas12/users/devita/clas12validation/clara-iss643-rich/plugins/clas12/bin/recon-util -i " + fileIN + " -o " + fileOUT + " -y " + yalm
     stdout = t.runcommand(command)
     print(stdout[0])
 
