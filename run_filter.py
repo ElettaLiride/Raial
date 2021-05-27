82230e5e42cb6611e6195522f0c2fa8de54ff6cf
ElettaLiride <giovanni.costantini92@gmail.com>
2021-05-27 10:21:18 +0200

    solving running problem

--- run_filter.py
+++ run_filter.py
@@ -8,13 +8,13 @@ from costantini_code import tools as t
 def runcommand(filetofilter, Layer="-1", eventsnumber=" "):
     f = os.path.basename(filetofilter)
     runnumber = t.getrunnumber(f)
-    command = "./mirazita_code/RichAI_FilterC/filterHipo -n" + eventsnumber + "- R" + runnumber + " -L " + Layer + " " + filetofilter
+    command = "./mirazita_code/RichAI_FilterC/filterHipo -n" + eventsnumber + "-R" + runnumber + " -L " + Layer + " " + filetofilter
     stdout = t.runcommand(command)
     print(stdout[0])
 
-    command = "mv rec_clas_" + runnumber + "AIskim1.hipo output/filter/"
+    command = "mv rec_clas_" + runnumber + "_AIskim1.hipo output/filter/"
     stdout = t.runcommand(command)
-    command = "mv rec_clas_" + runnumber + "AIskim1_events.out output/filter/"
+    command = "mv rec_clas_" + runnumber + "_AIskim1_events.out output/filter/"
     stdout = t.runcommand(command)
 
 
