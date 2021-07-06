faaa492802152117bcfe37785231161d1c230564
ElettaLiride <giovanni.costantini92@gmail.com>
2021-07-06 15:10:56 +0200

    testing tpe

--- TPE_opt.py
+++ TPE_opt.py
@@ -40,8 +40,28 @@ def pass_dict_param_to_table(dict, table):
     return table
 
 
-def objective(params):
+def objective(space):
     # COMPUTING SCORE
+    params = {
+        'dx_401': float(space['dx_401']),
+        'dy_401': float(space['dy_401']),
+        'dz_401': float(space['dz_401']),
+        'dthx_401': float(space['dthx_401']),
+        'dthy_401': float(space['dthy_401']),
+        'dthz_401': float(space['dthz_401']),
+        'dx_201': float(space['dx_201']),
+        'dy_201': float(space['dy_201']),
+        'dz_201': float(space['dz_201']),
+        'dthx_201': float(space['dthx_201']),
+        'dthy_201': float(space['dthy_201']),
+        'dthz_201': float(space['dthz_201']),
+        'dx_202': float(space['dx_202']),
+        'dy_202': float(space['dy_202']),
+        'dz_202': float(space['dz_202']),
+        'dthx_202': float(space['dthx_202']),
+        'dthy_202': float(space['dthy_202']),
+        'dthz_202': float(space['dthz_202'])
+    }
 
     # CHANGING PARAM ON CCDB
     old_pars_table = cc.reading_ccdb(provider, calibration_table, variation)
@@ -49,14 +69,14 @@ def objective(params):
     toadd = old_pars_table.values.tolist()
     cc.adding_to_ccdb(toadd, provider, calibration_table, variation)
 
-    start_time = time.time()
+    # start_time = time.time()
     # RUN EVENTBUILDER
     print("-----------------------------------------------------------------------------------------------")
     print("----------------------------------- START RECO -----------------------------------------")
     print("-----------------------------------------------------------------------------------------------")
     run_reco.runcommand(fileforreco)
-    reco_time = int(time.time() - start_time)
-    second_time = time.time()
+    # reco_time = int(time.time() - start_time)
+    # second_time = time.time()
 
 
     # RUN ANGLE ANALYSIS
@@ -65,13 +85,13 @@ def objective(params):
     print("-----------------------------------------------------------------------------------------------")
 
     run_plots.runcommand(fileforplot)
-    plot_time = int(time.time() - second_time)
+    # plot_time = int(time.time() - second_time)
 
     # SCORING
     score = make_mean(filefromplot)
-
+    #, reco_time, plot_time
     print("score: ", score)
-    return score, reco_time, plot_time
+    return score
 
 
 if __name__ == '__main__':
