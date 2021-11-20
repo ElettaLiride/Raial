27d1bde05227ed2a26e374309ec02920b3110806
ElettaLiride <giovanni.costantini92@gmail.com>
2021-11-20 18:14:02 +0100

    correct error

--- optimization/opt_firstlayer.py
+++ optimization/opt_firstlayer.py
@@ -4,6 +4,6 @@ from bo_gp import BoRichGp
 from objective import obj_gp
 
 if __name__=="__main__":
-    SPACE = Space.from_yaml('../SPM+201.yaml')
+    SPACE = Space.from_yaml('SPM+201.yaml')
     opt = BoRichGp(obj=obj_gp, space=SPACE, dir='../output/opt', id='layerone')
     opt.optimize(call=20)
