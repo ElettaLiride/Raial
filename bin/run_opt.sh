d90c682aa12b35064d3d847a074ca476cc0927ad
ElettaLiride <giovanni.costantini92@gmail.com>
2021-12-16 11:19:41 +0100

    coninuing cleaning code

--- bin/run_opt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source setup.sh
-
-export PYTHONPATH=$PYTHONPATH:/w/hallb-scshelf2102/clas12/users/costantini/RICH_alignment/
-
-python scripts/gp_minimize.py spaces/space.yaml 1 test
-
-swif -output
