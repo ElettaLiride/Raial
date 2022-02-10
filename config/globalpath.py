64277305b7933e6ead346504e500e601b87a2858
ElettaLiride <giovanni.costantini92@gmail.com>
2022-02-10 13:01:19 +0100

    globalpath change recon-util exe if we are in farm

--- config/globalpath.py
+++ config/globalpath.py
@@ -13,11 +13,16 @@ BINDIR = f'{RICHGEOAL}/bin'
 
 # calibration connection
 CALIBRATION_TABLE = "/calibration/rich/misalignments"
-VARIATION = "subtest"
+VARIATION = "scan_variation_15"
 USER = "anonymous"
 CALIBRATION_CONNECTION = f'sqlite:///{RICHGEOAL}/config/ccdb_4.3.2.sqlite'
 
 # optimization run
+if RICHGEOAL.split(sep='/')[1] == 'mnt':
+    RECOUTIL = 'recon-util'
+else:
+    RECOUTIL = '/work/clas12/users/devita/rich/oldVersionWithNewGeo/clas12-offline-software/coatjava/bin/recon-util'
+
 RN = 1
 ITER = 0
 STARTING_TABLE = pd.read_table('config/allzero_python.txt', sep='-')
