907e2129e87a1839c0e3795e593503e3357f3544
ElettaLiride <giovanni.costantini92@gmail.com>
2022-03-25 11:41:01 +0100

    smoothing setup configuration. Now a copy of the snapshot is created at every setup. 1 argument for name of snapshot needed

--- setup.sh
+++ setup.sh
@@ -3,6 +3,15 @@ export RICHGEOAL=$PWD
 export PYTHONPATH=$PYTHONPATH:$RICHGEOAL
 export PATH=$PATH:/$RICHGEOAL/bin
 
+if [[ ! -f "$RICHGEOAL/config/ccdbsnapshot/ccdb_4.3.2.sqlite"]]
+then
+  wget https://clasweb.jlab.org/clas12offline/sqlite/ccdb/clas12tags/cccdb_4.3.2.sqlite
+  mv cccdb_4.3.2.sqlite $RICHGEOAL/config/ccdbsnapshot
+fi
+
+cp $RICHGEOAL/config/ccdbsnapshot/ccdb_4.3.2.sqlite $RICHGEOAL/config/ccdbsnapshot/ccdb_$1.sqlite
+export CCDB_CONNECTION=sqlite:///$RICHGEOAL/config/ccdbsnapshot/ccdb_$1.sqlite
+
 ## SetUp for jlab farm
 if [[ -d "/group/" ]]
 then
@@ -12,14 +21,13 @@ module load anaconda3
 module load hipo/1.3
 
 export HIPO4ROOT=$HIPO
-export HIPO4LIB=$HIPO4ROOT/lib
-export HIPO4INC=$HIPO4ROOT/hipo4
-export LZ4DIR=$HIPO4ROOT/lz4
-
 
 source /w/hallb-scshelf2102/clas12/users/costantini/ccdb/environment.bash
 conda activate /w/hallb-scshelf2102/clas12/users/costantini/RICH_alignment/alignment-env/
 
 fi
 
-export CCDB_CONNECTION=sqlite:///$1
+export HIPO4LIB=$HIPO4ROOT/lib
+export HIPO4INC=$HIPO4ROOT/hipo4
+export LZ4DIR=$HIPO4ROOT/lz4
+
