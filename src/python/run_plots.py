c300bd8a4782c8c7c2f4ee23c52c466d47e819b1
ElettaLiride <giovanni.costantini92@gmail.com>
2022-03-30 18:07:17 +0200

    some print in run_plots

--- src/python/run_plots.py
+++ src/python/run_plots.py
@@ -17,16 +17,22 @@ def run_plot(input):
 
     """
     files = ""
-
+    print(input)
     if os.path.isdir(input):
         for file in os.listdir(input):
             if os.path.isfile(f'{input}/{file}'):
+                print(file)
+                print(file.split(sep='_')[4])
+                print(file.split(sep='_')[5].split('.')[0])
+                print(globalpath.ITER)
+                print(globalpath.RN)
                 if int(file.split(sep='_')[4]) == globalpath.ITER and int(file.split(sep='_')[5].split('.')[0]) == globalpath.RN:
                     files += " "
                     files += f'{input}/{file}'
     else:
        files = " " + input
     #print(files)
+    print(files)
     _ = t.runcommand(f"{globalpath.BINDIR}/{globalpath.RICHPLOTBIN} -R{globalpath.RN} {files}", output=globalpath.PRINTplot)
     _ = t.runcommand(f"mv RichPlots_{globalpath.RN}.out {globalpath.PLOTDIR}/result_{globalpath.RN}_{globalpath.ITER}.out")
     _ = t.runcommand(f"mv RichPlots_{globalpath.RN}.root {globalpath.PLOTDIR}/result_{globalpath.RN}_{globalpath.ITER}.root")
