725933c9289fa59ab3103ccfae0dea893f9c92fd
ElettaLiride <giovanni.costantini92@gmail.com>
2022-04-04 14:44:24 +0200

    some cleaning

--- scripts/opt_firstlayer.py
+++
@@ -1,9 +0,0 @@
-from skopt import Space
-
-from bo_gp import BoRichGp
-from objective import obj_gp
-
-if __name__=="__main__":
-    SPACE = Space.from_yaml('/work/clas12/users/costantini/RICH_alignment/SPM+201.yaml')
-    opt = BoRichGp(obj=obj_gp, space=SPACE, dir='/work/clas12/users/costantini/RICH_alignment/output/opt', id='layerone')
-    opt.optimize(call=1)
